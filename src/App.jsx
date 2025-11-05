7673ff694c648cec2bd13389bc47c59b9a48ae13
manuelba1997 <manuelbrugues@gmail.com>
2025-11-05 13:27:02 -0500

    Restar to blank

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import React from 'react'
 function App() {
-  const result = React.useState("Yes");
-  console.log(result);
+
 
   return (
 <main>
-  <h1 className='title'>Is state important to know?</h1>
-  <button className='value'>{result[0]}</button>
+
 </main>
   )
 }
