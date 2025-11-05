dd260acef8f3c634e94e7b6ee3a6cbbae307a426
manuelba1997 <manuelbrugues@gmail.com>
2025-11-05 14:22:35 -0500

    Restar to blank

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import React from 'react'
 function App() {
-
-
+  const [isGoingOut, setIsGoingOut] = React.useState(true)
   return (
 <main>
-
+<h1 className='title'>Do I feel like going out tonight</h1>
+<button  className='value'>Add Item</button>
 </main>
   )
 }
