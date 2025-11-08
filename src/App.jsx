0cb0029bce97796ea628e4ca5a0af7a8d51c076f
manuelba1997 <manuelbrugues@gmail.com>
2025-11-08 18:49:09 -0500

    Restar to blank -2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import React from 'react'
 function App() {
-  const [isGoingOut, setIsGoingOut] = React.useState(true)
+const [myFavrityThings, setMyFavoriteThings] = React.useState([]);
+
+const allMyFavrityThings = ["💦🌹","😺","💡🫖", "🔥🧤", "⚽🎁", "🐕", "🍎🥧", "🔔🚪", "🛎️🫑", "🥩🍝"];
+const thingElements = myFavrityThings.map(thing => <p key={thing}>{thing}</p>)
+function addFavoriteThing() {
+  // Pick a random item from allMyFavrityThings
+  setMyFavoriteThings(prevFabThings => [...prevFabThings, allMyFavrityThings[prevFabThings.length]])
+}
   return (
 <main>
 <h1 className='title'>Do I feel like going out tonight</h1>
-<button  className='value'>Add Item</button>
+<button onClick={addFavoriteThing} className='value'>Add Item</button>
+
+<section aria-live='polite'>
+{thingElements}
+</section>
 </main>
   )
 }
